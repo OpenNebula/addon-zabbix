5af1f0d2e6a5ad29ae6a8d8d228116978cd957ca
kvaps <kvapss@gmail.com>
2017-12-05 15:52:33 +0100

    add: one.sh

---
+++ one.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+
+ONEVM_FILE=/tmp/onevm.out
+ONEHOST_FILE=/tmp/onehost.out
+ONEDATASTORE_FILE=/tmp/onedatastore.out
+ONEVNET_FILE=/tmp/onevnet.out
+
+# Select, sum or average values from all resources in the list
+scount() {
+    awk "{ if( \"$1\" == \"avg\" || \"$1\" == \"\" ) {sum+=$ 2} else {if($ 1 == \"$1\") {printf(\"%.0f\n\", $ 2)}} } END { if( \"$1\" == \"avg\" ) { printf(\"%.0f\n\", sum/NR) } if( \"$1\" == \"\" ) { printf(\"%.0f\n\", sum) } }"
+}
+
+percent(){
+    echo "$1" "$2" | awk '{printf("%.0f\n", $1*100/$2)}'
+}
+
+difference(){
+    echo "$1" "$2" | awk '{printf("%.0f\n", $1-$2)}'
+}
+
+list_hosts_param() {
+    xmlstarlet sel -s -t -c "//HOST_POOL/HOST/ID/text()|//HOST_POOL/HOST/HOST_SHARE/$1" -n $ONEHOST_FILE | sed -e 's/<[^/<]*>/\t/g' -e 's/<\/[^<]*>/\n/g'
+}
+
+hosts_mem() {
+    if [ "$#" == 2 ]; then
+        list_hosts_param "$1" | scount "$2" | awk '{ printf( "%.0f\n", $1 * 1048576)}'
+    else
+        OPERATION="$1"
+        PARAM1="$(hosts_mem "$2" "$4")"
+        PARAM2="$(hosts_mem "$3" "$4")"
+        $OPERATION $PARAM1 $PARAM2
+    fi
+}
+
+hosts_cpu() {
+    if [ "$#" == 2 ]; then
+        list_hosts_param "$1" | scount "$2"
+    else
+        OPERATION="$1"
+        PARAM1="$(hosts_cpu "$2" "$4")"
+        PARAM2="$(hosts_cpu "$3" "$4")"
+        $OPERATION $PARAM1 $PARAM2
+    fi
+}
+
+hosts_count() {
+    if [ -z "$1" ]; then
+        xmlstarlet sel -s -t -v "/*/*/ID" -n $ONEHOST_FILE | wc -l
+    else
+        xmlstarlet sel -s -t -c '//HOST_POOL/HOST/ID/text()|//HOST_POOL/HOST/STATE' -n $ONEHOST_FILE | sed -e 's/<[^/<]*>/\t/g' -e 's/<\/[^<]*>/\n/g' | \
+          awk '{init=0; monitoring_monitored=1; monitored=2; error=3; disabled=4; monitoring_error=5; monitoring_init=6; monitoring_disabled=7; offline=8}{ if( $ 2 == '$1' ){print $ 1} }' | wc -l
+    fi
+}
+
+vms_count() {
+    if [ -z "$1" ]; then
+        xmlstarlet sel -s -t -v "/*/*/ID" -n $ONEVM_FILE | wc -l
+    else
+        xmlstarlet sel -s -t -c '//VM_POOL/VM/ID/text()|//VM_POOL/VM/STATE|//VM_POOL/VM/LCM_STATE' -n /tmp/onevm.out | \
+            sed -e 's/<\/LCM_STATE>/\n/g' -e 's/\(<[^<]*>\)\{1,\}/\t/g' -e 's/\t\t//' | \
+            awk '{init=0; pending=1; hold=2; active=3; stopped=4; suspended=5; poweroff=8; undeployed=9; clonning=10; split("16 19 60", lcm_unknown);
+            split("36 37 38 39 40 41 42 44 46 47 48 49 50 61", lcm_failed) }
+            { if("'$1'" == "unknown"){ for (i=1;i in lcm_unknown;i++) if($ 3 == lcm_unknown[i]){print $ 1}}}
+            { if("'$1'" == "failed"){ if($ 3 == 7 ||$ 3 == 11){print $ 1}}}
+            { if("'$1'" == "failed"){ for (i=1;i in lcm_failed;i++) if($ 3 == lcm_failed[i]){print $ 1}}}
+            { if( !("'$1'" == "failed") && !("'$1'" == "unknown") && $ 2 == '$1' ){print $ 1} }' | wc -l
+    fi
+}
+
+datastores_count() {
+    xmlstarlet sel -s -t -v "/*/*/ID" -n $ONEDATASTORE_FILE | wc -l
+}
+
+vnets_count() {
+    xmlstarlet sel -s -t -v "/*/*/ID" -n $ONEVNETS_FILE | wc -l
+}
+
+case "$1" in
+    hosts_count      ) hosts_count      "$2" ;;
+    vms_count        ) vms_count        "$2" ;;
+    datastores_count ) datastores_count "$2" ;;
+    vnets_count      ) vnets_count      "$2" ;;
+
+    hosts_total_mem ) hosts_mem TOTAL_MEM "$2" ;;
+    hosts_free_mem  ) hosts_mem FREE_MEM  "$2" ;;
+    hosts_used_mem  ) hosts_mem USED_MEM  "$2" ;;
+    hosts_mem_usage ) hosts_mem MEM_USAGE "$2" ;;
+    hosts_total_cpu ) hosts_cpu TOTAL_CPU "$2" ;;
+    hosts_free_cpu  ) hosts_cpu FREE_CPU  "$2" ;;
+    hosts_used_cpu  ) hosts_cpu USED_CPU  "$2" ;;
+    hosts_cpu_usage ) hosts_cpu CPU_USAGE "$2" ;;
+    hosts_pfree_mem ) hosts_mem percent FREE_MEM TOTAL_MEM "$2" ;;
+    hosts_pused_mem ) hosts_mem percent USED_MEM TOTAL_MEM "$2" ;;
+    hosts_pfree_cpu ) hosts_cpu percent FREE_CPU TOTAL_CPU "$2" ;;
+    hosts_pused_cpu ) hosts_cpu percent USED_CPU TOTAL_CPU "$2" ;;
+    hosts_pcpu_usage ) hosts_cpu percent CPU_USAGE TOTAL_CPU "$2" ;;
+    hosts_pmem_usage ) hosts_cpu percent MEM_USAGE TOTAL_MEM "$2" ;;
+    hosts_pused_mem_usage ) hosts_cpu percent USED_CPU CPU_USAGE "$2" ;;
+    hosts_pused_cpu_usage ) hosts_cpu percent USED_MEM MEM_USAGE "$2" ;;
+    hosts_unused_mem ) hosts_mem difference MEM_USAGE USED_MEM "$2" ;;
+    hosts_unused_cpu ) hosts_cpu difference CPU_USAGE USED_CPU "$2" ;;
+esac
+
